1d48c8e1b952bf843af32e97a14480d3e89730b5
cibss <cibss13@gmail.com>
2022-10-01 15:27:04 +0700

    configure emotion cache

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
-import Head from "next/head";
 import { ThemeProvider } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import Head from "next/head";
 import CssBaseline from "@mui/material/CssBaseline";
+import createEmotionCache from '../utils/createEmotionCache';
 import theme from "../utils/theme";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
-    <>
+    <CacheProvider value={emotionCache}>
       <Head>
         <title>Netflix</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
@@ -15,7 +22,7 @@ function MyApp({ Component, pageProps }) {
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </CacheProvider>
   );
 }
 
